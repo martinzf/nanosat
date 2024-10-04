39b40f59ffeb2bbfd189224e62e8e9ea179de544
Martin Zapata <martzapa@ucm.es>
2024-10-04 20:45:07 +0200

    Modified tests, added simulink control

--- testbench/testbench.ino
+++ testbench/testbench.ino
@@ -5,21 +5,11 @@
 BLEService IMU9DOF("180C");
 BLEService RWHEELS("181C");
 
-// BLE IMU characteristics
-BLEFloatCharacteristic ble_ax("2A56", BLERead | BLENotify);
-BLEFloatCharacteristic ble_ay("2A57", BLERead | BLENotify);
-BLEFloatCharacteristic ble_az("2A58", BLERead | BLENotify);
-BLEFloatCharacteristic ble_gx("2A59", BLERead | BLENotify);
-BLEFloatCharacteristic ble_gy("2A60", BLERead | BLENotify);
-BLEFloatCharacteristic ble_gz("2A61", BLERead | BLENotify);
-BLEFloatCharacteristic ble_mx("2A62", BLERead | BLENotify);
-BLEFloatCharacteristic ble_my("2A63", BLERead | BLENotify);
-BLEFloatCharacteristic ble_mz("2A64", BLERead | BLENotify);
-// BLE Wheel characteristics
-BLEUnsignedCharCharacteristic ble_w1("2A65", BLEWrite | BLENotify);
-BLEUnsignedCharCharacteristic ble_w2("2A66", BLEWrite | BLENotify);
-BLEUnsignedCharCharacteristic ble_w3("2A67", BLEWrite | BLENotify);
-BLEUnsignedCharCharacteristic ble_w4("2A68", BLEWrite | BLENotify);
+// BLE IMU characteristic (1D array of floats, for accel (g) + gyro (dps) + mag data (uT))
+BLECharacteristic ble_imu("2A56", BLERead | BLENotify, sizeof(float) * 9);
+
+// BLE Wheels characteristic (1D array of unsigned chars for 4 wheels)
+BLECharacteristic ble_whl("2A65", BLEWrite | BLENotify, 4);
 
 void setup() {
   // Start serial transmission
@@ -45,20 +35,10 @@ void setup() {
   BLE.setAdvertisedService(RWHEELS);
     
   // Add characteristics to IMU BLE Service 
-  IMU9DOF.addCharacteristic(ble_ax);
-  IMU9DOF.addCharacteristic(ble_ay);
-  IMU9DOF.addCharacteristic(ble_az);
-  IMU9DOF.addCharacteristic(ble_gx);
-  IMU9DOF.addCharacteristic(ble_gy);
-  IMU9DOF.addCharacteristic(ble_gz);
-  IMU9DOF.addCharacteristic(ble_mx);
-  IMU9DOF.addCharacteristic(ble_my);
-  IMU9DOF.addCharacteristic(ble_mz);
+  IMU9DOF.addCharacteristic(ble_imu);
+
   // Add characteristics to wheels BLE Service
-  RWHEELS.addCharacteristic(ble_w1);
-  RWHEELS.addCharacteristic(ble_w2);
-  RWHEELS.addCharacteristic(ble_w3);
-  RWHEELS.addCharacteristic(ble_w4);
+  RWHEELS.addCharacteristic(ble_whl);
 
   // Add services to the BLE stack
   BLE.addService(IMU9DOF);
@@ -70,9 +50,9 @@ void setup() {
 }
 
 void loop() {
-  float a[3]; // Acceleration 
-  float g[3]; // Angular velocity
-  float m[3]; // Magnetometer
+  float a[3];         // Acceleration 
+  float g[3];         // Angular velocity
+  float m[3];         // Magnetometer
   unsigned char w[4]; // Wheel PWM commands
 
   // Check if central device is connected
@@ -90,21 +70,10 @@ void loop() {
       }
 
       // Write IMU data to characteristics
-      ble_ax.writeValue(a[0]);
-      ble_ay.writeValue(a[1]);
-      ble_az.writeValue(a[2]);
-      ble_gx.writeValue(g[0]);
-      ble_gy.writeValue(g[1]);
-      ble_gz.writeValue(g[2]);
-      ble_mx.writeValue(m[0]);
-      ble_my.writeValue(m[1]);
-      ble_mz.writeValue(m[2]);
+      ble_imu.writeValue(a, sizeof(a) + sizeof(g) + sizeof(m));
 
       // Read, execute and print wheel PWM commands
-      ble_w1.readValue(w[0]);
-      ble_w2.readValue(w[1]);
-      ble_w3.readValue(w[2]);
-      ble_w4.readValue(w[3]);
+      ble_whl.readValue(w, sizeof(w));
       analogWrite(A0, w[0]);
       analogWrite(A1, w[1]);
       analogWrite(A2, w[2]);
